988b7ef3d0b19c762a4cbda35fe23f01ef4d6d82
kali-octavio1504 <kali.octavio@gmail.com>
2024-08-06 13:36:40 -0600

    Se agregan rutas al proyecto

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
+Route::view('contacto', 'contact')->name('contact');
+Route::view('blog', 'blog')->name('blog');
+Route::view('nosotros', 'about')->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
